fa94b1a255fe678c9f0beacc07ca90f26fd92bb3
Deimos451 <minkevichdenis@gmail.com>
2020-09-29 22:59:09 +0300

    Update settings.py

--- denmin/settings.py
+++ denmin/settings.py
@@ -123,9 +123,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
