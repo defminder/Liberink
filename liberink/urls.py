dec11c449bed2cb5dd11db21ef127a59e752e8dd
Deimos451 <minkevichdenis@gmail.com>
2020-11-19 17:01:23 +0300

    rename

--- liberink/urls.py
+++ liberink/urls.py
@@ -1,4 +1,4 @@
-"""denmin URL Configuration
+"""liberink URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
