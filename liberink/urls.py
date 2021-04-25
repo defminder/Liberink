bf57244341e75563924ac6a0883e5a9d83482cc9
d.minkevich <minkevichdenis@gmail.com>
2021-04-25 22:46:45 +0300

    fix unused url usage

--- liberink/urls.py
+++ liberink/urls.py
@@ -3,7 +3,6 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('grappelli/', include('grappelli.urls')), # grappelli URLS
     path('admin/', admin.site.urls), # admin site
     path('', include('main.urls')),
     path('', include('register.urls')),
