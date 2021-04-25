2384c5380d4631b4a1c6e32b2354ebd956862f6d
d.minkevich <minkevichdenis@gmail.com>
2021-04-26 00:37:45 +0300

    PreLoader screen animation
    Minor updates:
    - fix font size at welcome page

---
+++ static/JS/pre_loader.js
@@ -0,0 +1,4 @@
+window.onload = function(){
+    document.getElementsByClassName('page-header')[0].style.position = 'fixed';
+    document.getElementsByClassName('pre_loader_wrapper')[0].remove();
+};
